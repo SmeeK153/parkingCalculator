d452aa9ffc2666869f4f190aad275fb5c39bcc28
Eric Freeman <EricSmeeFreeman@hotmail.com>
2015-11-17 20:35:26 -0800

    Running through another integration tutorial, setup initial recommended packages.

---
+++ seleniumTestAutomation/seleniumTestAutomation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seleniumTestAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the reference for the browser
+            IWebDrive
+        }
+    }
+}
