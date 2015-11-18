a1150eba31a56876a5fe1cb2bf7ce6716952863d
Eric Freeman <EricSmeeFreeman@hotmail.com>
2015-11-17 20:47:42 -0800

    Added chromedriver.exe after system ignored it.

--- SeleniumClassLibrary/SeleniumClassLibrary/Class1.cs
+++ SeleniumClassLibrary/SeleniumClassLibrary/Class1.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.PhantomJS;
-using OpenQA.Selenium.Support.UI;
 
 namespace WebdriverTest
 {
---
+++ automationTests/automationTests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.PhantomJS;
+
+
+namespace automationTests
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver d = new ChromeDriver();
+            d.Navigate().GoToUrl("www.google.com");
+              }
+
+    }
+}
