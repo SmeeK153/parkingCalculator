a5cc05143754fde3e1eb4354600fcf55e81bf2fe
Eric Freeman <EricSmeeFreeman@hotmail.com>
2015-11-17 19:26:20 -0800

    Adding test code from Visual Studio Selenium integration instructions.

---
+++ SeleniumClassLibrary/SeleniumClassLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebdriverTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver d = new ChromeDriver();
+            d.Navigate().GoToUrl(“http://techinews24.com”);
+              }
+    }
+}
